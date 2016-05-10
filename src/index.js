018b96d23bdff6c6dd96ae8db1a9cde6e1c3cdaf
Enoah Netzach <f.castellarin@gmail.com>
2016-05-10 13:29:18 +0100

    export transform function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import path from 'path';
 import fs from 'fs';
 
-export default function({ types }) {
+export const transform = (envs, defaultEnv = 'development', importRelPath, filename) => {
+  const envExt = envs[process.env.NODE_ENV || defaultEnv];
+
+  if (typeof envExt === 'undefined') {
+    return;
+  }
+
+  const dirname = path.dirname(filename);
+  const importedFilename = path.normalize(`${dirname}/${importRelPath}`);
+  const importedExtname = path.extname(importedFilename);
+
+  const envDirname = path.dirname(importedFilename);
+  const envExtname = importedExtname || '.js';
+  const envBasename = path.basename(importedFilename, envExtname);
+
+  const envFilename = `${envDirname}/${envBasename}.${envExt}${envExtname}`;
+
+  fs.accessSync(envFilename);
+
+  const newImportDirname = path.dirname(importRelPath);
+  const newImportBasename = path.basename(importRelPath, importedExtname);
+  return `${newImportDirname}/${newImportBasename}.${envExt}${importedExtname}`;
+};
+
+export default ({ types }) => {
   return {
     visitor: {
       ImportDeclaration(nodePath, state) {
@@ -9,39 +33,20 @@ export default function({ types }) {
           production: 'prod',
           development: 'dev'
         };
-        const defaultEnv = state.opts.defaultEnv || 'development';
-
-        const envExt = envs[process.env.NODE_ENV || defaultEnv];
-
-        if (typeof envExt === 'undefined') {
-          return;
-        }
 
         const { node } = nodePath;
-        const importRelPath = node.source.value;
-
-        const dirname = path.dirname(state.file.opts.filename);
-        const importedFilename = path.normalize(`${dirname}/${importRelPath}`);
-        const importedExtname = path.extname(importedFilename);
-
-        const envDirname = path.dirname(importedFilename);
-        const envExtname = importedExtname || '.js';
-        const envBasename = path.basename(importedFilename, envExtname);
-
-        const envFilename = `${envDirname}/${envBasename}.${envExt}${envExtname}`;
 
         try {
-          fs.accessSync(envFilename);
+          node.source = types.stringLiteral(transform(
+            envs,
+            state.opts.defaultEnv,
+            node.source.value,
+            state.file.opts.filename
+          ));
         } catch (err) {
-          return;
+          // will use default import statement
         }
-
-        const newImportDirname = path.dirname(importRelPath);
-        const newImportBasename = path.basename(importRelPath, importedExtname);
-        const newImportFilename = `${newImportDirname}/${newImportBasename}.${envExt}${importedExtname}`;
-
-        node.source = types.stringLiteral(newImportFilename);
       }
     }
   }
-};
+}
